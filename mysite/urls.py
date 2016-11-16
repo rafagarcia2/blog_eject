f3a5ae1f19c9ec2d43f3d5fef20f57fafbc5eeac
Rafael <rafaeldantas@eject.com.br>
2016-11-15 22:53:18 -0300

    new front-end applied

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls', namespace='blog')),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('blog.urls', namespace='blog')),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
